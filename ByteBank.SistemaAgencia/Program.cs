19fe31b7d40c1ebbeddfc0df83bf47bd91ff6898
Carol Ramos <carolineramos98@outlook.com>
2021-08-12 16:28:38 -0300

    Conclusão do terceiro tópico do curso 6 de Orientação a Objetos

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -13,21 +13,119 @@ namespace ByteBank.SistemaAgencia
     {
         public static void Main(string[] args)
         {
+            string urlTeste = "https://www.bytebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
 
-            DateTime dataFimPagamento = new DateTime(2021, 08, 11);
-            DateTime dataCorrente = DateTime.Now;
+            urlTeste.StartsWith("https://www.bytebank.com");
+            urlTeste.EndsWith("https://www.bytebank.com");
 
-            TimeSpan diferenca = TimeSpan.FromMinutes (60); /*dataFimPagamento - dataCorrente;
-*/
-            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com"));             /*StartWith: começa com*/
+            Console.WriteLine(urlTeste.EndsWith("cambio"));                                 /*EndsWith: termina com*/
+            Console.WriteLine(urlTeste.Contains("ByteBank"));                               /*Contains: contém*/
 
-            Console.WriteLine(mensagem);
+            //Console.WriteLine(indiceByteBank == 0);
 
+            Console.ReadLine();
+
+
+            //pagina? argumentos
+            //0123456789
+
+            //moedaOrigem = real & moedaDestino = dolar
+
+
+            //Testando ToLower
+
+            string mensagemOrigem = "PALAVRA";
+            string termoBusca = "ra";
+
+            Console.WriteLine(mensagemOrigem.ToUpper());
+            Console.WriteLine(mensagemOrigem.ToLower());
+
+            //Testando Replace
+
+            termoBusca = termoBusca.Replace('r', 'R');
+            Console.WriteLine(termoBusca);
+
+            termoBusca = termoBusca.Replace('a', 'A');
+            Console.WriteLine(termoBusca);
+
+            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
+            Console.ReadLine();
+
+            string urlParametros = "https://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+
+            string valor = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + valor);
+
+            string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
+
+            Console.ReadLine();
+
+            //Testando o método Remove
+            string testeRemocao = "primeiraParte&parteParaRemover";
+            int indiceEComercial = testeRemocao.IndexOf('&');
+            Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
+
+            Console.ReadLine();
+
+
+            string palavra = "moedaOrigem=IReadOnlyList&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino=";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string " + nomeArgumento.Length);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length));
+
+            Console.ReadLine();
+
+
+
+
+
+            //Testando o IsNullOrEmpty
+            //string textoVazio = "";
+            //string textoNulo = null;
+            //string textoQualquer = "lkshfo";
+
+
+            //Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            //Console.WriteLine(string.IsNullOrEmpty(textoNulo));
+            //Console.WriteLine(string.IsNullOrEmpty(textoQualquer));
+
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            //string url = "pagina?argumentos";
+
+            //int indiceInterrogacao = url.IndexOf('?');
+
+            //Console.WriteLine(indiceInterrogacao);
+
+            //Console.WriteLine(url);
+            //string argumentos = url.Substring(indiceInterrogacao + 1);
+            //Console.WriteLine(argumentos);
+
+
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Fim da execução, tecle enter para sair...");
             Console.ReadLine();
 
         }
 
-        
+
 
     }
 }
