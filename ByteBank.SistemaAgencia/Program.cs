941a018deebf03e587b76e733a7d3a3423511d25
Carol Ramos <Carol Ramos@DESKTOP-D8D0U8O>
2021-08-12 12:56:54 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            DateTime dataFimPagamento = new DateTime(2021, 08, 11);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes (60); /*dataFimPagamento - dataCorrente;
+*/
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+
+        }
+
+        
+
+    }
+}
